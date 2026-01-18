5d909d14bcab80f99d293ed8e594354fe7095b21
owuorviny109 <owuorvincent958@gmail.com>
2026-01-18 12:40:34 +0300

    first commit

---
+++ CashFlowDashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CashFlowDashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
