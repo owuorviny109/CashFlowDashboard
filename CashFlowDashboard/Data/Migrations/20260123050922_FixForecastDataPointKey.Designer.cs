f7eac514a2d08a9efa1bfc48711eb7ffd8744d28
owuorviny109 <owuorvincent958@gmail.com>
2026-01-23 12:23:43 +0300

    feat(data): add migration to fix ForecastDataPointKey schema

--- CashFlowDashboard/Data/Migrations/20260123050922_FixForecastDataPointKey.Designer.cs
+++ CashFlowDashboard/Data/Migrations/20260123050922_FixForecastDataPointKey.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CashFlowDashboard.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CashFlowDashboard.Data.Migrations
 {
     [DbContext(typeof(CashFlowDbContext))]
-    partial class CashFlowDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260123050922_FixForecastDataPointKey")]
+    partial class FixForecastDataPointKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.0");
---
+++ CashFlowDashboard/Data/Migrations/20260123050922_FixForecastDataPointKey.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CashFlowDashboard.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixForecastDataPointKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ForecastDataPoint",
+                table: "ForecastDataPoint");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "ForecastDataPoint",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ForecastDataPoint",
+                table: "ForecastDataPoint",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ForecastDataPoint_ForecastScenarioId",
+                table: "ForecastDataPoint",
+                column: "ForecastScenarioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ForecastDataPoint",
+                table: "ForecastDataPoint");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ForecastDataPoint_ForecastScenarioId",
+                table: "ForecastDataPoint");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "ForecastDataPoint",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ForecastDataPoint",
+                table: "ForecastDataPoint",
+                columns: new[] { "ForecastScenarioId", "Id" });
+        }
+    }
+}
